ccfd349b5020592b583adbd7d0acee5f7c4b9913
Urvashi Singh <urvashi.singh2605@gmail.com>
2023-04-08 12:47:04 +0530

    go routine

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+func Print(str string, ch chan int) {
+	fmt.Println(str)
+	ch <- 1
+}
+
 func main() {
 	fmt.Println("hello Urvashi")
 	fmt.Println("hello Ashish")
+	ch := make(chan int)
+	go Print("Ashish", ch)
+	<-ch
 }
